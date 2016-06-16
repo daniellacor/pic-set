ef7170698110fde2be7ff61ed7e77b5a7196e1d2
daniellacor <daniella.corricelli@gmail.com>
2016-06-16 14:21:25 -0400

    intro page and routes complete'

--- app/router.js
+++ app/router.js
@@ -9,7 +9,9 @@ Router.map(function() {
   this.route('lightbox');
   this.route('account');
   this.route('intro');
-  this.route('lessons');
+  this.route('lessons', function() {
+    this.route('lesson');
+  });
   this.route('user');
   this.route('score');
   this.route('flashcard');
