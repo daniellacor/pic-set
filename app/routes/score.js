e0f29ac88db1a33908c69ef8fa956f67a881ed8e
daniellacor <daniella.corricelli@gmail.com>
2016-06-20 11:38:52 -0400

    scoring update

--- app/routes/score.js
+++ app/routes/score.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service('session'),
+  var session = this.get('session')
+  return this.store.findRecord('score', 'session.userId')
 });
