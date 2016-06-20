cf9849209a14decc7ee7aae73aacee41bf82d1c5
daniellacor <daniella.corricelli@gmail.com>
2016-06-20 14:11:02 -0400

    scoring moved to lesson controller

--- app/controllers/lessons/lesson.js
+++ app/controllers/lessons/lesson.js
@@ -3,7 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     judge(settings) {
-      debugger
+
+    },
+    score(points){
+      session: Ember.inject.service('session-account'),
+        var account = this.get('session.account.data')
+        var user_id = account.id
+        this.store.findRecord('user', user_id).then(function(score){
+          user.set('score', points);
+          user.save();
+        }
+
+
     }
   }
 });
--- app/routes/score.js
+++ app/routes/score.js
@@ -1,7 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  session: Ember.inject.service('session'),
-  var session = this.get('session')
-  return this.store.findRecord('score', 'session.userId')
+
 });
