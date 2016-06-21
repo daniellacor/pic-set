5f20730ec7b9f4703df7bdefd10d573ca081b352
daniellacor <daniella.corricelli@gmail.com>
2016-06-21 12:55:05 -0400

    scoring update

--- app/routes/lessons/lesson.js
+++ app/routes/lessons/lesson.js
@@ -1,35 +1,28 @@
+// import Ember from 'ember';
+//
+// export default Ember.Route.extend({
+//   session: Ember.inject.service('session-account'),
+//   model(params) {
+//     return this.store.findRecord('lesson', params.lesson_id)
+//   }
+// });
+
+
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   session: Ember.inject.service('session-account'),
   model(params) {
-    return this.store.findRecord('lesson', params.lesson_id)
+    var session_id = this.get('session.account.data.id')
+    return Ember.RSVP.hash({
+      lesson: this.store.findRecord('lesson', params.lesson_id),
+      score: this.store.createRecord('score', {user: session_id, lesson: params.lesson_id, score: 0})
+    });
+  },
+
+  setupController(controller, models) {
+    controller.set('lesson', models.lesson);
+    controller.set('score', models.score);
   }
-  afterModel() {
-    this.set('score', this.modelFor('score'))
-  }
-  // model(params) {
-    // return Ember.RSVP.hash({
-      // lesson: this.store.findRecord('lesson', params.lesson_id),
-      // flashcards: this.store.findRecord('flashcard', 1),
-      // flashcards: this.store.query('flashcard', {filter: {lessonId: this.get(params.lesson_id)}}).then(response){
-        // return response
-      // },
-      // score: this.store.createRecord('score')
-      // score: this.store.query('score', {filter: {userId: this.get('session.account.data.id')}}).then(function(response){
-        // if (response.content.length == 0) {
-          // return that.store.createRecord('score')
-        // } else {
-          // response.query( 'score', {filter: {lessonId: params.lesson_id}}).then(function(response){
-            // if (response.content.length == 0) {
-              // return that.store.createRecord('score')
-            // } else {
-              // return response
-            // }
-          // })
-        // }
-        // console.log(response)
-      // })
-    // })
-  // }
+
 });
